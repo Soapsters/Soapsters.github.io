3ea96dda1eb519b1756acab815a88150b453f577
Soapsters <78459897+Soapsters@users.noreply.github.com>
2021-11-01 21:56:25 -0700

    Create HeaderIncludes.php

---
+++ blog/MasterPages/HeaderIncludes.php
@@ -0,0 +1,7 @@
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/~srank20/blog/style/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/~srank20/blog/style/csslibs/bootstrap.min.css">
+  <script src="/~srank20/blog/javascript/jslibs/jquery.min.js"></script>
+  <script src="/~srank20/blog/javascript/jslibs/bootstrap.min.js"></script>
+  <script src="/~srank20/blog/javascript/jslibs/awesomefont.min.js"></script>
