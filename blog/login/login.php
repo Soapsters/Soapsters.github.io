e834e0be133d0c29ee971fd21a2b8099bec3773f
Soapsters <78459897+Soapsters@users.noreply.github.com>
2021-11-01 21:55:17 -0700

    Create login.php

---
+++ blog/login/login.php
@@ -0,0 +1,114 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: ../index.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM blog_user WHERE username = ?";
+        
+        if($statement = $db->prepare($sql)){            
+            // Set parameters
+            $statement->execute([$username]);
+
+			//get the username and password from the database
+			$results = $statement->fetch(PDO::FETCH_ASSOC);
+			$tempjson = json_encode($results);
+			$newresult = json_decode($tempjson, true);
+			$dbusername = $newresult['username'];
+			$dbpassword = $newresult['password'];
+			$dbid = $newresult['id'];
+            // Attempt to execute the prepared statement
+			
+			//we could add password hashes here, but moving on...
+			if($dbpassword == $password){
+				// Password is correct, so start a new session
+				//session_start();
+				
+				// Store data in session variables
+				$_SESSION["loggedin"] = true;
+				$_SESSION["id"] = $dbid;
+				$_SESSION["username"] = $dbusername;                            
+				
+				// Redirect user to index page
+				header("location: ../index.php");
+			} else{
+				// Password is not valid, display a generic error message
+				$login_err = "Invalid username or password.";
+			}
+			                       }
+    }
+    }
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
