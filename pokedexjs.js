54e88ba9dbb1f7756e89cbff961edc10f8ee2e27
Soapsters <78459897+Soapsters@users.noreply.github.com>
2021-12-02 13:58:53 -0800

    Update pokedexjs.js

--- pokedexjs.js
+++ pokedexjs.js
@@ -1,5 +1,6 @@
 url = 'https://pokeapi.co/api/v2/pokemon/';
 
+//set connection to the URL and await for fetch of values
 document.getElementById('enterButton').onclick = async function() {
   let val = document.getElementById('pokemonName').value;
   console.log(val);
@@ -7,18 +8,21 @@ let response = await fetch(url + val);
 console.log(response.status);
 let json = await response.json();
 
+//Create a statements to pull out the first type and the second type (if there is a second type)
 const dataTypes = json.types;
 const dataFirstType = dataTypes[0];
 const dataSecondType = dataTypes[1];
 const pokeTypeOne = document.getElementById('typeOne');
 const pokeTypeTwo = document.getElementById('typeTwo');
 pokeTypeOne.innerHTML = dataFirstType.type.name;
+//Create a if statement to display the second type if it exists or empty the string to 
 if(dataSecondType) {
     pokeTypeTwo.innerHTML = dataSecondType.type.name;
 } else {
     pokeTypeTwo.innerHTML = '';
 }
 
+//create response to output the data from the API
 document.getElementById('response').innerHTML = '<b>' + json.name + 
 '</b><br><img src="' + json.sprites.front_default + '" />' 
 + '</b><br><img src="' + json.sprites.back_default + '" />'
