ffd4eb0b28c716835d1ed05394c4b942b3e455f4
Soapsters <78459897+Soapsters@users.noreply.github.com>
2021-12-02 01:03:15 -0800

    Update pokedexjs.js

--- pokedexjs.js
+++ pokedexjs.js
@@ -5,10 +5,25 @@ document.getElementById('enterButton').onclick = async function() {
   console.log(val);
 let response = await fetch(url + val);
 console.log(response.status);
-
 let json = await response.json();
-console.log(json);
+
+const dataTypes = json.types;
+const dataFirstType = dataTypes[0];
+const dataSecondType = dataTypes[1];
+const pokeTypeOne = document.getElementById('typeOne');
+const pokeTypeTwo = document.getElementById('typeTwo');
+pokeTypeOne.innerHTML = dataFirstType.type.name;
+if(dataSecondType) {
+    pokeTypeTwo.innerHTML = dataSecondType.type.name;
+} else {
+    pokeTypeTwo.innerHTML = '';
+}
+
 document.getElementById('response').innerHTML = '<b>' + json.name + 
-'</b><br><img src="' + json.sprites.front_default + '" />' + '<br><h3> Type: ' + json.types[0].type.name + '</h3></b>'
+'</b><br><img src="' + json.sprites.front_default + '" />' 
++ '</b><br><img src="' + json.sprites.back_default + '" />'
++ '<br><h3> Type: ' + pokeTypeOne.innerHTML + '</h3></b>' 
++ '<h3> Type 2: ' + pokeTypeTwo.innerHTML + '</h3></b>'
 + '<h3> Height: ' + json.height + ' dm </h3>' + '<h3> Weight: ' + json.weight + ' hg </h3>';
+
 }
