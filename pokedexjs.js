ce24356871d3d1935958de3f7e8d52c4a7bff2a2
Soapsters <78459897+Soapsters@users.noreply.github.com>
2021-12-02 19:26:35 -0800

    Update pokedexjs.js

--- pokedexjs.js
+++ pokedexjs.js
@@ -23,11 +23,11 @@ if(dataSecondType) {
 }
 
 //create response to output the data from the API
-document.getElementById('response').innerHTML = '<b>' + json.name + 
-'</b><br><img src="' + json.sprites.front_default + '" />' 
+document.getElementById('response').innerHTML = '<b>' + json.name 
++ '</b><br><img src="' + json.sprites.front_default + '" />' 
 + '</b><br><img src="' + json.sprites.back_default + '" />'
-+ '<br><h3> Type: ' + pokeTypeOne.innerHTML + '</h3></b>' 
++ '</b><h3> ID: #' + json.id + '</h3>'
++ '<h3> Type: ' + pokeTypeOne.innerHTML + '</h3></b>' 
 + '<h3> Type 2: ' + pokeTypeTwo.innerHTML + '</h3></b>'
 + '<h3> Height: ' + json.height + ' dm </h3>' + '<h3> Weight: ' + json.weight + ' hg </h3>';
-
 }
